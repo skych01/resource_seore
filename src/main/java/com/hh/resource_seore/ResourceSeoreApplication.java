ba788e1937992c0017d0dca4c6af028af07f22d9
469690029@qq.com <qq469690029>
2019-09-02 13:22:08 +0800

    增加接口说明功能

--- src/main/java/com/hh/resource_seore/ResourceSeoreApplication.java
+++ src/main/java/com/hh/resource_seore/ResourceSeoreApplication.java
@@ -1,34 +1,48 @@
 package com.hh.resource_seore;
 
+import io.swagger.annotations.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 
 @SpringBootApplication
-@RestController
-public class ResourceSeoreApplication  extends SpringBootServletInitializer {
+@Controller
+@Api(value = "index", description = "资源相关api")
+public class ResourceSeoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceSeoreApplication.class, args);
     }
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
         return builder.sources(ResourceSeoreApplication.class);
     }
 
-    @RequestMapping("/updateFile")
-    public String test(String fileName, @RequestParam("file") MultipartFile file, HttpServletRequest request) {
-        String path = ImageTools.getAppPath(request) ,serverPath="";
+
+    @RequestMapping(value = "/updateFile", method = RequestMethod.POST)
+    @ApiOperation(value = "上传文件资源")
+    @ResponseBody
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "fileName", value = "book's name", required = true),
+            @ApiImplicitParam(name = "file", value = "book's date", required = false)})
+    public String test(String fileName,
+                       @RequestParam("file") MultipartFile file,
+                       HttpServletRequest request) {
+        String path = ImageTools.getAppPath(request), serverPath = "";
 
         for (int i = 0; i < 3; i++) {
             serverPath = serverPath + CreateBasicData.getRandomString(3);
@@ -50,20 +64,27 @@ public class ResourceSeoreApplication  extends SpringBootServletInitializer {
     }
 
 
-    @RequestMapping("/updateFile4fileName")
-    public String test1(String fileName,String imgPath, HttpServletRequest request) {
+    @RequestMapping(value = "/updateFile4fileName", method = RequestMethod.POST)
+    @ApiOperation(value = "上传文件资源")
+    @ApiParam()
+    @ResponseBody
+    public String test1(String fileName, String imgPath, HttpServletRequest request) {
         String path = ImageTools.getAppPath(request), serverPath = "";
         for (int i = 0; i < 3; i++) {
-            serverPath = serverPath + CreateBasicData.getRandomString(3);
+            serverPath = serverPath + CreateBasicData.getRandomString(i);
             serverPath += "/";
         }
-        File m = new File(path + serverPath);
-        System.out.println(path + serverPath);
         try {
             DownloadImg.download(imgPath, path + serverPath + fileName, path + serverPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return serverPath+fileName;
+        return serverPath + fileName;
+    }
+
+    @RequestMapping("/")
+    public void index(HttpServletResponse response) {
+        response.setStatus(302); //设置响应行的状态码为302 重定向
+        response.setHeader("Location", "swagger-ui.html#");
     }
 }
